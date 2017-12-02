aa4c4d2991491738cfd00f7f758d2a32e9b4c8d4
paul.rutledge <paul.rutledge@vodori.com>
2017-12-02 15:10:46 -0600

    cleanup!

--- test/clj_embed/core_test.clj
+++ test/clj_embed/core_test.clj
@@ -1,5 +1,16 @@
 (ns clj-embed.core-test
   (:require [clojure.test :refer :all]
-            [clj-embed.core :refer :all]))
+            [clj-embed.core :refer :all])
+  (:import (clojure.lang RT)))
 
 
+
+(deftest isolated-rt
+  (testing "When I get the hash code of RT in the same runtime, they are the same."
+    (let [[h1 h2] (let [r (new-runtime)] [(with-runtime (.hashCode RT)) (with-runtime (.hashCode RT))])]
+      (is (= h1 h2))))
+
+  (testing "When I get the hash code of RT across two different runtimes, they are different."
+    (let [h1 (-> (new-runtime) (with-runtime (.hashCode RT)))
+          h2 (-> (new-runtime) (with-runtime (.hashCode RT)))]
+      (is (not= h1 h2)))))
